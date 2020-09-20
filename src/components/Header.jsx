c3ee2f8bc545ce579847400ada66df0cbac9f059
marleyanthony <marleycanthony@gmail.com>
2020-09-20 13:43:53 -0700

    created main.css file. added global styles. added variables

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="header">
+      <h2 className="header__where">Where in the world?</h2>
+      <div className="header__dark-mode"></div>
+    </div>
+  )
+}
+
+export default Header
